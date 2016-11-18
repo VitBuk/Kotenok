79ed8b916c6a5b53fba27ade1187132c444469eb
VitBuk <vitbuk@gmail.com>
2016-11-18 21:10:55 +0200

    POST/GET tries

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ app.get('/', function(req, res) {
     res.send('Hello world!');
 });
 
-app.listen(3000, function() {
+// app.get('/test', function(req, res) {
+//     res.send('En Taro Adun!');
+// });
+
+app.get('/test', function(req,res) {
+    var kotenok = req.query.kotenok;
+    res.send(kotenok);
+});
+
+app.post('/postTest', function(req, res){
+    res.send('En Taro Andun!');
+});
+
+app.listen(process.env.PORT || 3000, function() {
     console.log('Listening to 3000');
 });
