567c2bd527e53a7d348b29753450d4c06f7e6095
VitBuk <vitbuk@gmail.com>
2016-12-02 00:15:43 +0200

    ajax increment

--- app.js
+++ app.js
@@ -11,40 +11,52 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.get('/table', function(req, res) {
+app.get('/table', function (req, res) {
     res.render('table');
 });
 
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
     res.render('about');
 });
 
-app.get('/increase', function(req, res) {
+app.get('/increase', function (req, res) {
     res.render('increase');
 });
 
-app.get('/increase2', function(req, res) {
+app.get('/increase2', function (req, res) {
     res.render('increase2');
 });
 
-app.get
+
+app.get('/increase3', function (req, res) {
+    //req.query.value
+    res.render('increase3');
+
+});
+
+app.get('/increase3ZHE', function (req, res) {
+    var value = req.query.value;
+    value = parseInt(value) + 1;
+    res.send(value + "");
+});
+
 // app.get('/test', function(req, res) {
 //     res.send('En Taro Adun!');
 // });
 
-app.get('/test', function(req,res) {
+app.get('/test', function (req, res) {
     var kotenok = req.query.kotenok;
     res.send(kotenok);
 });
 
-app.post('/postTest', function(req, res){
+app.post('/postTest', function (req, res) {
     res.send('En Taro Andun!');
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Listening to 3000');
 });
