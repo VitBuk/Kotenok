7243b021c2768c8981e795655519c4aecafe8e01
XLIII <xliii@mail.com>
2016-12-18 00:29:06 +0200

    Footer fixed

--- app.js
+++ app.js
@@ -7,9 +7,12 @@
  */
 
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.render('index');
