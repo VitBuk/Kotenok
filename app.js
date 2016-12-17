324d7ca07126885c63b4bb0506177d79ba092288
XLIII <xliii@mail.com>
2016-12-18 00:15:49 +0200

    Cleanup

--- app.js
+++ app.js
@@ -15,42 +15,8 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.get('/table', function (req, res) {
-    res.render('table');
-});
-
 app.get('/about', function (req, res) {
     res.render('about');
-});
-
-app.get('/increase', function (req, res) {
-    res.render('increase');
-});
-
-app.get('/increase2', function (req, res) {
-    res.render('increase2');
-});
-
-app.get('/increase3', function (req, res) {
-    res.render('increase3');
-});
-
-app.get('/increase3ZHE', function (req, res) {
-    var value = parseInt(req.query.value) + 1;
-    res.send(value.toString());
-});
-
-app.get('/test', function (req, res) {
-    var kotenok = req.query.kotenok;
-    res.send(kotenok);
-});
-
-app.post('/postTest', function (req, res) {
-    res.send('En Taro Andun!');
-});
-
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Listening to 3000');
 });
 
 app.get('/create', function (req, res) {
@@ -101,17 +67,6 @@ app.locals.games = [];
 app.locals.gameId = 0;
 app.locals.playerId = 0;
 
-/*    Entities
-
- var player = {
- "id": "int",
- "name": "string",
- "score": "int"
- };
-
- var game = {
- "id": "int",
- "players": "players",
- "state": "state"
- };
- */
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Listening to 3000');
+});
